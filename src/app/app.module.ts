9c9684df86ff3d54a1a3dc3034854b4435735f8d
sagars0600 <sagarsolanki2120@gmail.com>
2023-07-21 17:25:53 +0530

    Fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,26 +4,21 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EmployeeListComponent } from './views/employee-module/employee-list/employee-list.component';
-import { AddEmployeeComponent } from './views/employee-module/add-employee/add-employee.component';
-import { EditEmployeeComponent } from './views/employee-module/edit-employee/edit-employee.component';
-import { ViewEmployeeComponent } from './views/employee-module/view-employee/view-employee.component';
-
-
+import { AuthModuleModule } from './views/auth-module/auth-module.module';
+import { EmployeeModuleModule } from './views/employee-module/employee-module.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmployeeListComponent,
-    AddEmployeeComponent,
-    EditEmployeeComponent,
-    ViewEmployeeComponent,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    AuthModuleModule,
+    EmployeeModuleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/views/employee-module/employee-module.module.ts
+++ src/app/views/employee-module/employee-module.module.ts
@@ -2,11 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EmployeeRoutingModule } from './employee-module-routing.module';
 import { FormsModule } from '@angular/forms';
+import { AddEmployeeComponent } from './add-employee/add-employee.component';
+import { ViewEmployeeComponent } from './view-employee/view-employee.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { EditEmployeeComponent } from './edit-employee/edit-employee.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AddEmployeeComponent,
+    ViewEmployeeComponent,
+    EmployeeListComponent,
+    EditEmployeeComponent
+  ],
   imports: [
     CommonModule,
     EmployeeRoutingModule,
